2f2f714ca3db613aa7820f8896c3170f7ae31bd5
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-24 19:38:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,9 +7,9 @@ const
 function onKeyDownHandler(e){
   const key = parseInt(e.detail || e.which)
 
-  if(key === code[index]){
-    index++;
-  }
+    if(key === code[index]){
+      index++;
+    }
 
     if(index ===code.length){
       alert("Hurray!");
