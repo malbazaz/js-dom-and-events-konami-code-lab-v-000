18c80a7d5e88b87096159376e4a6991d428036a6
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-24 19:56:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,7 @@ function onKeyDownHandler(e){
     if(key === code[index]){
       index++;
       if(index === code.length){
-        alert("Hurray!");
+        window.alert("Hurray!");
         index = 0;
       } else {
         index = 0;
