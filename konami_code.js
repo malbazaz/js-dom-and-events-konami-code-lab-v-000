773d3af3d093b5af0a5778ba28f90b372fe8201c
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-24 20:21:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,20 +4,18 @@ function init() {
   // Write your JavaScript code inside the init() function
 let index = 0;
 // const input = document.querySelector('input')
-
-
 document.body.addEventListener('keydown', function(e){
-    const key = parseInt(e.detail || e.which)
-      debugger;
-      console.log(key)
-      if(key === code[index]){
-        index++;
-        if(index === code.length){
-          window.alert("Hurray!");
-          index = 0;
-        } else {
-          index = 0;
+        const key = parseInt(e.detail || e.which)
+        // debugger;
+        console.log(key)
+        if(key === code[index]){
+          index++;
+          if(index === code.length){
+            window.alert("Hurray!");
+            index = 0;
+          } else {
+            index = 0;
+          }
         }
-      }
-    } )
+      } )
 }
