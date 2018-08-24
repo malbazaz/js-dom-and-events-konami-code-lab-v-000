91d1706668f66cc9458e7890a3eecf73c78840ed
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-24 19:51:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ function init() {
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
   // Write your JavaScript code inside the init() function
 let index = 0;
-let input = document.querySelector('input')
+const input = document.querySelector('input')
 function onKeyDownHandler(e){
     const key = parseInt(e.detail || e.which)
     if(key === code[index]){
